953a19c2ef6f7dac0e7b8f46712391a6b3460b8d
Zidan Kharisma <zidan.kharisma@ui.ac.id>
2022-08-07 23:33:15 +0700

    test(orders): create test

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 import express, { Request, Response } from "express";
-import { validateRequest } from "@zpyon/common";
+import { BadRequestError, NotFoundError, OrderStatus, validateRequest } from "@zpyon/common";
 import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -18,6 +20,29 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError("Ticket is already reserved");
+    }
+
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+    await order.save();
+
     res.status(201).send();
   }
 );
